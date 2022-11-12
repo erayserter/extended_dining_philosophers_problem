d1f0f13dc9971572a6c07c581eabf54943bb523a
Mehmet Eray Serter <mehmeterayserter@gmail.com>
2022-11-13 00:41:53 +0300

    Last version

--- dining_philosophers.c
+++ dining_philosophers.c
@@ -5,21 +5,30 @@
 #include <time.h>
 #include <stdlib.h>
 
-void startDine();
-void* dine(void* philospherID);
+void initNewTurn();
+void* startDine(void* philosopherID);
+void dine(long int philospherID);
 void diningCycle(long int philosopherID);
-bool enterAyranQueueAndGet(long int philosopherID);
-void enterEatingQueueAndEat(long int philosopherID);
+bool waitAndGetAyran(long int philosopherID);
+void waitAndEat(long int philosopherID);
 void pickupChopstick(long int philosopherID);
 void putdownChopstick(long int philosopherID);
 void testPhilosopherCanEat(int order);
 int findPhilosopher(long int philosopherID);
+void printInfo();
+void* setTurn();
+void init();
 
 typedef enum {ayranWaiting, hungry, eating, thinking} philosopherState;
+
 struct mutexInfo {
     pthread_t* tid;
     pthread_mutex_t ayranMutex;
     pthread_mutex_t chopstickMutex;
+    pthread_mutex_t garsonLock;
+    pthread_mutex_t garsonCallLock;
+    pthread_cond_t garsonCondition;
+    pthread_cond_t newTurnWait;
     pthread_cond_t* eatingCondition;
 } mutexInfo;
 
@@ -27,64 +36,63 @@ philosopherState* states;
 int* eatCount;
 int philosopherCount;
 int ayranLeft;
-
-void init() {
-    states = (philosopherState*) malloc(sizeof(philosopherState) * philosopherCount);
-    mutexInfo.tid = (pthread_t*) malloc(sizeof(pthread_t) * philosopherCount);
-    mutexInfo.eatingCondition = (pthread_cond_t*) malloc(sizeof(pthread_cond_t) * philosopherCount);
-    eatCount = (int*) malloc(sizeof(int) * philosopherCount);
-
-    pthread_mutex_init(&(mutexInfo.ayranMutex), NULL);
-    pthread_mutex_init(&(mutexInfo.chopstickMutex), NULL);
-
-    for(int i = 0;i < philosopherCount; i++) {
-        pthread_cond_init(&((mutexInfo.eatingCondition)[i]), NULL);
-        eatCount[i] = 0;
-        states[i] = thinking;
-    }
-}
+int totalAyran;
+int processNumber = 0;
 
 void startDineForNPhilosophers() {
-    ayranLeft = rand() % (5 * philosopherCount) + 1;
-    printf("total ayran count: %d\n\n", ayranLeft);
-
-    init();
+    pthread_t tid;
+    pthread_create(&tid, NULL, setTurn, (void*)&tid);
 
     for (int i = 0; i < philosopherCount; i++)
-        pthread_create(&(mutexInfo.tid[i]), NULL, dine, (void*)&(mutexInfo.tid[i]));
+        pthread_create(&(mutexInfo.tid[i]), NULL, startDine, (void*)&(mutexInfo.tid[i]));
 
     for (int i = 0; i < philosopherCount; i++)
         pthread_join(mutexInfo.tid[i], NULL);
-    
-    for (int i = 0; i < philosopherCount; i++)
-        printf("philosopher %d ate %d times.\n", i, eatCount[i]);
-
-    printf("=============================\n\nyeni tur basliyor\n\n=============================\n");
 }
 
-void startDine() {
-    while (true) {
-        startDineForNPhilosophers();
+void* setTurn() {
+    while(true) {
+        pthread_mutex_lock(&mutexInfo.garsonLock);
+        pthread_cond_wait(&mutexInfo.garsonCondition, &mutexInfo.garsonLock);
+
+        printInfo();
+
+        initNewTurn();
+
         sleep(5);
+
+        for (int i = 0; i < philosopherCount; i++)
+            pthread_cond_signal(&mutexInfo.newTurnWait);
+        pthread_mutex_unlock(&mutexInfo.garsonLock);
     }
 }
 
-void* dine(void* philosopherID) {
+void* startDine(void* philosopherID) {
     long int id = *((long int *)philosopherID);
-    while(ayranLeft > 0) {
-        diningCycle(id);
+    int order = findPhilosopher(id);
+    while (true) {
+        dine(id);
+        pthread_mutex_lock(&mutexInfo.garsonCallLock);
+        if (++processNumber == philosopherCount)
+            pthread_cond_signal(&mutexInfo.garsonCondition);
+        pthread_cond_wait(&mutexInfo.newTurnWait, &mutexInfo.garsonCallLock);
+        pthread_mutex_unlock(&mutexInfo.garsonCallLock);
     }
-    return philosopherID;
+}
+
+void dine(long int philosopherID) {
+    while(ayranLeft > 0) {
+        diningCycle(philosopherID);
+    }
 }
 
 void diningCycle(long int philosopherID) {
-    sleep(0);
-    bool hasAyran = enterAyranQueueAndGet(philosopherID);
+    bool hasAyran = waitAndGetAyran(philosopherID);
     if (hasAyran)
-        enterEatingQueueAndEat(philosopherID);
+        waitAndEat(philosopherID);
 }
 
-bool enterAyranQueueAndGet(long int philosopherID) {
+bool waitAndGetAyran(long int philosopherID) {
     states[findPhilosopher(philosopherID)] = ayranWaiting;
     pthread_mutex_lock(&(mutexInfo.ayranMutex));
     if (ayranLeft <= 0) {
@@ -92,18 +100,15 @@ bool enterAyranQueueAndGet(long int philosopherID) {
         return false;
     }
     ayranLeft--;
-    sleep(0);
     pthread_mutex_unlock(&(mutexInfo.ayranMutex));
     return true;
 }
 
-void enterEatingQueueAndEat(long int philosopherID) {
+void waitAndEat(long int philosopherID) {
     pickupChopstick(philosopherID);
     int order = findPhilosopher(philosopherID);
     eatCount[order]++;
-    sleep(0);
     putdownChopstick(philosopherID);
-    sleep(0);
 }
 
 void pickupChopstick(long int philosopherID) {
@@ -141,13 +146,50 @@ int findPhilosopher(long int philosopherID) {
     return -1;
 }
 
+void printInfo() {
+    printf("---------------------------\n");
+    printf("Surahide %d tas ayran var.\n\n", totalAyran);
+    for (int i = 0; i < philosopherCount; i++)
+        printf("Process %d, %d porsiyon yedi.\n", i, eatCount[i]);
+}
+
+void init() {
+    states = (philosopherState*) malloc(sizeof(philosopherState) * philosopherCount);
+    mutexInfo.tid = (pthread_t*) malloc(sizeof(pthread_t) * philosopherCount);
+    mutexInfo.eatingCondition = (pthread_cond_t*) malloc(sizeof(pthread_cond_t) * philosopherCount);
+    eatCount = (int*) malloc(sizeof(int) * philosopherCount);
+
+    pthread_mutex_init(&(mutexInfo.ayranMutex), NULL);
+    pthread_mutex_init(&(mutexInfo.chopstickMutex), NULL);
+    pthread_mutex_init(&(mutexInfo.garsonLock), NULL);
+    pthread_mutex_init(&(mutexInfo.garsonCallLock), NULL);
+
+    pthread_cond_init(&mutexInfo.garsonCondition, NULL);
+    pthread_cond_init(&mutexInfo.newTurnWait, NULL);
+
+    for (int i = 0; i < philosopherCount; i++)
+        pthread_cond_init(&((mutexInfo.eatingCondition)[i]), NULL);
+}
+
+void initNewTurn() {
+    for(int i = 0;i < philosopherCount; i++) {
+        eatCount[i] = 0;
+        states[i] = thinking;
+    }
+    totalAyran = ayranLeft = rand() % (5 * philosopherCount) + 1;
+    processNumber = 0;
+}
+
 int main() {
     srand(time(0));
 
     printf("Enter the number of philosophers: ");
     scanf("%d", &philosopherCount);
 
-    startDine();
+    init();
+    initNewTurn();
+
+    startDineForNPhilosophers();
 
     return 0;
 }
